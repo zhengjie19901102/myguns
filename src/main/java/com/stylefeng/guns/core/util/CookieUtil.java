dbd3a25017c52b5d18e4c95077e3e67040b81916
zhengjie19901102 <1058686106@qq.com>
2018-08-17 21:26:12 +0800

    Cookie工具类

---
+++ src/main/java/com/stylefeng/guns/core/util/CookieUtil.java
@@ -0,0 +1,21 @@
+package com.stylefeng.guns.core.util;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+public class CookieUtil {
+
+    /**
+     * 获取指定Cookie值
+     */
+    public static String getCookieValue(HttpServletRequest request,String cookieName){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie:cookies) {
+            String name = cookie.getName();
+            if(cookieName.equals(name)){
+                return cookie.getValue();
+            }
+        }
+        return null;
+    }
+}
